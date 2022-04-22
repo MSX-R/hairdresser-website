ee5b4456fabf40f14d2b79a5f3dcd995222eb458
Marsaleix Romain <msx.coach@gmail.com>
2022-04-22 17:05:57 +0200

    add change to the form text

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -69,7 +69,7 @@ function Formulaire() {
         <label htmlFor="message">Votre demande</label>
 
         <textarea
-          placeholder="N'hésitez pas à préciser le type de soin que vous souhaitez ainsi que le jour souhaité"
+          placeholder="N'hésitez pas à préciser le type de soin que vous souhaitez ainsi que le jour de rendez-vous désiré"
           name="user_message"
         ></textarea>
 
