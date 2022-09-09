f206426cadc3032c8f28302983a4d258272f6b77
Marsaleix Romain <msx.coach@gmail.com>
2022-09-09 16:08:52 +0200

    fix links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 // import des pages
-import Home from './screens/Home';
-import LeSalon from './screens/LeSalon';
-import Book from './screens/Book';
-import Prestations from './screens/Prestations';
-import Contact from './screens/Contact';
-// 
-import WelcomePage from './screens/WelcomePage';
-import Authentification from './screens/Authentification';
-// 
+import Home from "./screens/Home";
+import LeSalon from "./screens/LeSalon";
+import Book from "./screens/Book";
+import Prestations from "./screens/Prestations";
+import Contact from "./screens/Contact";
+//
+import WelcomePage from "./screens/WelcomePage";
+import Authentification from "./screens/Authentification";
+//
 // import Modal from './components/Modal';
 //
 
@@ -18,14 +18,13 @@ function App() {
     <div className="App">
       {/* <Modal /> A REACTIVER SI BOUTON OK NAV CONTACT*/}
       <Routes>
-        <Route path="/accueil" element={<Home />} />
-        <Route path="/le-salon" element={<LeSalon />} />
-        <Route path="/book-photo" element={<Book />} />
-        <Route path="/prestations" element={<Prestations />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/authentification" element={<Authentification />} />
         <Route path="/" element={<WelcomePage />} />
-
+        <Route path="/hairdresser-website/accueil" element={<Home />} />
+        <Route path="/hairdresser-website/le-salon" element={<LeSalon />} />
+        <Route path="/hairdresser-website/book-photo" element={<Book />} />
+        <Route path="/hairdresser-website/prestations" element={<Prestations />} />
+        <Route path="/hairdresser-website/contact" element={<Contact />} />
+        <Route path="/hairdresser-website/authentification" element={<Authentification />} />
       </Routes>
     </div>
   );
--- src/screens/WelcomePage.js
+++ src/screens/WelcomePage.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import './WelcomePage.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./WelcomePage.css";
+import { Link } from "react-router-dom";
 
-// import logoTransp from '../assets/logo-transp.png'
-import logoTransp from '../assets/Logo/logo-blanc-or.png'
-
+import logoTransp from "../assets/Logo/logo-blanc-or.png";
 
 function WelcomePage() {
   return (
     <div className="page-welcome">
       {/* <Link to="/authentification" className="WelcomeConnexion">Connexion</Link> */}
-        <div className="top-welcome"></div>
-        
-        <Link to="/home">
-          <div className="AdelineCoiffureLogo" title="Cliquez pour entrer sur le site">
-            {/* Adeline */}
-          <img src={logoTransp} alt="logo transparent" className="logo-welcome-page" />
-          
-<div className="nom-entreprise-logo">L'ATELIER D'ADELINE
-</div>
+      <div className="top-welcome"></div>
 
-<div className="ville-logo">- Antibes -</div>
+      <Link to="/hairdresser-website/accueil">
+        <div
+          className="AdelineCoiffureLogo"
+          title="Cliquez pour entrer sur le site"
+        >
+          <img
+            src={logoTransp}
+            alt="logo transparent"
+            className="logo-welcome-page"
+          />
 
-</div></Link>
- <div className="bottom-welcome"></div>
+          <div className="nom-entreprise-logo">L'ATELIER D'ADELINE</div>
 
-
+          <div className="ville-logo">- Antibes -</div>
+        </div>
+      </Link>
+      <div className="bottom-welcome"></div>
     </div>
-  )
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
